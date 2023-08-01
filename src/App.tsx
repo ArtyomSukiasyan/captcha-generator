dae3651627f1ad50df55975bb44c56404040331c
Artyom Sukiasyan <artiom.suqiasyan1401@gmail.com>
2023-08-01 20:39:27 +0400

    feat: make simple ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
 import "./App.css";
+import RefreshIcon from "./assets/RefreshIcon";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="container">
+      <header>Captcha Generator</header>
+      <div className="input_field captcha_box">
+        <input type="text" value="" disabled />
+        <button className="refresh_button">
+          <RefreshIcon />
+        </button>
+      </div>
+      <div className="input_field captcha_input">
+        <input type="text" placeholder="Enter captcha" />
+      </div>
+      <div className="message">Entered captcha is correct</div>
+      <div className="input_field button disabled">
+        <button>Submit Captcha</button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
